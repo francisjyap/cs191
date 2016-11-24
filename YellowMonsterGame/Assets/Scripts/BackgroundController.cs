eb0aa52a3d3ed7b0797bd9203dd995542b7f4db2
francisjyap <francisj.yap@gmail.com>
2016-11-24 14:51:35 +0800

    11-24 finished
    finished output daily

---
+++ YellowMonsterGame/Assets/Scripts/BackgroundController.cs
@@ -0,0 +1,23 @@
+﻿using UnityEngine;
+using System.Collections;
+
+public class BackgroundController : MonoBehaviour {
+
+	public float moveRate = .07f;
+
+	// Use this for initialization
+	void Start () {
+		
+	}
+	
+	// Update is called once per frame
+	void FixedUpdate () {
+		transform.position = new Vector2 (transform.position.x - moveRate, transform.position.y);
+
+		if (transform.position.x <= -27.51) {
+			transform.position = new Vector2 (27.51f, transform.position.y);
+		}
+	}
+
+}
+
