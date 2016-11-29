34f8479861fb7a71f4853ea2e86488bda4d4db6d
francisjyap <francisj.yap@gmail.com>
2016-11-29 14:53:39 +0800

    11-29 finished

---
+++ YellowMonsterGame/Assets/Scripts/EnemyController.cs
@@ -0,0 +1,22 @@
+﻿using UnityEngine;
+using System.Collections;
+
+public class EnemyController : MonoBehaviour {
+
+	public float batMoveRate = .2f;
+
+
+	// Use this for initialization
+	void Start () {
+	
+	}
+	
+	// Update is called once per frame
+	void FixedUpdate () {
+		transform.position = new Vector2 (transform.position.x - batMoveRate, transform.position.y);
+
+		if (transform.position.x <= -15) {
+			transform.position = new Vector2 (Random.Range(15,25), Random.Range(-4, 4));
+		}
+	}
+}
---
+++ YellowMonsterGame/Assets/Scripts/PlayerController.cs
@@ -0,0 +1,40 @@
+﻿using UnityEngine;
+using System.Collections;
+
+public class PlayerController : MonoBehaviour {
+
+	public float moveRate = 0.2f;
+	public bool isDead = false;
+	public float lowerBound = -4f;
+	public float upperBound = 4f;
+	public float leftBound = -8.5f;
+	public float rightBound = 8.5f;
+
+	// Use this for initialization
+	void Start () {
+		
+	}
+	
+	// Update is called once per frame
+	void FixedUpdate () {
+		if (isDead == false) {
+			float moveX = Input.GetAxis("Horizontal");
+			float moveY = Input.GetAxis ("Vertical");
+
+			transform.position = new Vector2 (transform.position.x + moveX, transform.position.y + moveY);
+
+			if (transform.position.y >= upperBound) {
+				transform.position = new Vector2 (transform.position.x, upperBound);
+			}
+			if (transform.position.y <= lowerBound) {
+				transform.position = new Vector2 (transform.position.x, lowerBound);
+			}
+			if (transform.position.x <= leftBound) {
+				transform.position = new Vector2 (leftBound, transform.position.y);
+			}
+			if (transform.position.x >= rightBound) {
+				transform.position = new Vector2 (rightBound, transform.position.y);
+			}
+		}
+	}
+}
